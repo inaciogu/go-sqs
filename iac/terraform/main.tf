a1f017b6bf851b8f24e5f5c4cfe42360dd9d1fcc
Gustavo <gus.inacio@hotmail.com>
2024-01-07 12:24:01 -0300

    chore: update iac to test locally

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -12,24 +12,39 @@ provider "aws" {
   }
 }
 
-module "sns_topic_subscription" {
-  source = "github.com/paulo-tinoco/terraform-sns-topic-subscription"
+resource "aws_sqs_queue" "test" {
+  name = "test"
+}
 
-  topics = [
-    {
-      name = "example"
-    }
-  ]
-
-  queues = [
-    {
-      name = "example_queue"
-      topics_to_subscribe = [
-        {
-          name = "example"
+resource "aws_sqs_queue_policy" "test" {
+  queue_url = aws_sqs_queue.test.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "Allow-SNS-SendMessage"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
         }
-      ]
-    }
-  ]
-  account_id = "000000000000"
+        Action = "sqs:SendMessage"
+        Resource = aws_sqs_queue.test.arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sns_topic.test.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_sns_topic" "test" {
+  name = "test"
+}
+
+resource "aws_sns_topic_subscription" "test" {
+  topic_arn = aws_sns_topic.test.arn
+  protocol = "sqs"
+  endpoint = aws_sqs_queue.test.arn
 }
