9a56458ba5f70f41b479a94670ec2888c0349086
Gustavo <gus.inacio@hotmail.com>
2024-01-11 09:17:07 -0300

    feat: create messagemodel

---
+++ messagemodel/message.go
@@ -0,0 +1,48 @@
+package messagemodel
+
+import (
+	"encoding/json"
+)
+
+type MessageAttributes map[string]Attribute
+
+type Attribute struct {
+	Type  string
+	Value string
+}
+
+type MessageMetadata struct {
+	MessageId         string
+	ReceiptHandle     string
+	MessageAttributes map[string]string
+}
+
+type SNSMessageBody struct {
+	MessageAttributes MessageAttributes
+	Message           string
+}
+
+type Message struct {
+	Content  string
+	Metadata MessageMetadata
+}
+type User struct {
+	Name string `json:"name"`
+}
+
+func New(content string, metadata MessageMetadata) *Message {
+	return &Message{
+		Content:  content,
+		Metadata: metadata,
+	}
+}
+
+func (m *Message) Unmarshal(v interface{}) error {
+	err := json.Unmarshal([]byte(m.Content), v)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
